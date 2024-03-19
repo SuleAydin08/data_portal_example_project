f4a6132291cb5170caa548a6fb2db81b8bd8512c
SuleAydin08 <160034825+SuleAydin08@users.noreply.github.com>
2024-03-19 21:21:57 +0300

    Widgetta const verildi

--- lib/pharmacy_widget.dart
+++ lib/pharmacy_widget.dart
@@ -1,12 +1,11 @@
 import 'dart:convert';
-import 'package:data_portal_example_project/pharmacy_model.dart';
 import 'package:data_portal_example_project/pharmacy_list.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 // ignore: must_be_immutable
 class PharmacyWidget extends StatefulWidget {
-  PharmacyWidget({
+  const PharmacyWidget({
     super.key,
   });
 
